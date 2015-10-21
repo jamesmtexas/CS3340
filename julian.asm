1990a6911ae60f3ffeed16716178acee082e00d8
James Murphy <jmm140330@utdallas.edu>
2015-10-21 12:14:35 -0500

    Just need to handle output now

--- julian.asm
+++ julian.asm
@@ -23,8 +23,9 @@ main:
 	
 	#1 in $v0 if leap year, else 0
 	
-	add	$t0, $v0, $zero	#Save return value from leap year checker so we can use $v0 again
-	
+	add	$s0, $v0, $zero	#Save return value from leap year checker so we can use $v0 again
+	add	$s1, $a0, $zero	#Also save the julian nummber	
+
 	li	$v0, 4
 	la	$a0, NumberPrompt
 	syscall
@@ -35,11 +36,78 @@ main:
 	
 	subi	$t2, $t1, 366
 	
-	beq	$t2, $t0 ,Invalid
+	beq	$t2, $s0 ,Invalid
 
-	j Exit
+	#naive solution: hard code the values of each month (adding 1 after feb in leap years)
+	#see if assembly lets you define constants
+	#sub the month value, if less than 0 it is that month + day
+	#repeat, subtracting the *cumulative* month values (oh god there will be so many conditional branches)
+	#wait no fuck that, just keep a variable with the count of the month we're on and increment each time
+	#so we still have a lot of cases but at least the thing we're subtracting is clear
+
+	#Leap year or not - $s0
+	#Julian number - $s1
+
+	#use $t0 for month count
+	addi	$t0, $t0, 31
+	sub	$t1, $t0, $s1
+	blz	$t1, January
 	
+	beqz	$s0, LeapTrack
 
+NonLeapTrack:
+	addi	$t0, $t0, 28
+	j	AfterLeap	
+
+LeapTrack:
+	addi	$t0, $t0, 29
+	j	AfterLeap	
+	
+AfterLeap: specific month labels will print their own output then exit	
+	sub	$t1, $t0, $s1
+	blz	$t1, February
+
+	addi	$t0, $t0, 31
+	sub	$t1, $t0, $s1
+	blz	$t1, March
+
+	addi    $t0, $t0, 30
+        sub     $t1, $t0, $s1
+        blz     $t1, April 
+
+	addi    $t0, $t0, 31
+        sub     $t1, $t0, $s1
+        blz     $t1, May
+
+	addi    $t0, $t0, 30
+        sub     $t1, $t0, $s1
+        blz     $t1, June
+
+	addi    $t0, $t0, 31
+        sub     $t1, $t0, $s1
+        blz     $t1, July
+
+	addi    $t0, $t0, 31
+        sub     $t1, $t0, $s1
+        blz     $t1, August
+
+	addi    $t0, $t0, 30
+        sub     $t1, $t0, $s1
+        blz     $t1, September
+
+	addi    $t0, $t0, 31
+        sub     $t1, $t0, $s1
+        blz     $t1, October
+
+	addi    $t0, $t0, 30
+        sub     $t1, $t0, $s1
+        blz     $t1, November
+
+	addi    $t0, $t0, 31
+        sub     $t1, $t0, $s1
+        blz     $t1, December
+	
+	j Exit
 
 #Following subprogram determines whether a given year is a leap year
 S1:
